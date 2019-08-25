8d18d2d519b74be7552015b7ee742e063c5be1ee
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-08-25 23:16:53 +0300

    Python variables

---
+++ day3.py
@@ -0,0 +1,26 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4 # x is of type int
+x = "sally" # x is now of type str
+print(x)
+x = "John"
+# is the same as
+x = 'John'
+#variables
+x, y, z = "Red", "Black", "Green"
+print(x)
+print(y)
+print(z)
+#Output variables
+x = "Easy"
+print("Python is "+x)
+x="Python is "
+y="easy"
+z=x+y
+print(z)
+#combine str with int will give an error in Python
+x=5
+y="five"
+print(x+y) #Error 
