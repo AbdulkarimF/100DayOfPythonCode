e14ed1de8e303b319042ff191a5d6870975df984
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-08-25 23:17:46 +0300

    Python numbers

---
+++ day4.py
@@ -0,0 +1,45 @@
+#Python Numbers
+x=1 #int
+y=2.8 #float
+z=1j #complex
+# type() function
+print(type(x))
+print(type(y))
+print(type(z))
+#int
+x=1
+y=2876776465865
+z=-6785874874765
+print(type(x))
+print(type(y))
+print(type(z))
+#float
+x=1.10
+y=2.0
+z=-50.25
+print(type(x))
+print(type(y))
+print(type(z))
+x=35e3
+y=12E4
+z=-87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+#complex
+#Type conversion functions
+x= 1
+y= 2.5
+z= 1j
+a= float(x)   #convert from int into float
+b= int(y)     #convert from float into into
+c= complex(x) #convert from int into complex
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+#Random number function
+import random
+print(random.randrange(1,10)) 
