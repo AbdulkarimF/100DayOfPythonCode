1dc0bbb7e8f06f548061224b6899e6ebb7718010
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-08-27 02:10:31 +0300

    Python strings 2

---
+++ Day8.py
@@ -0,0 +1,25 @@
+#Python strings 2
+print('Example 1')
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, world!"
+
+print('Example 2')
+a = "Hello, World!"
+print(len(a))
+
+print('Example 3')
+a = 'HELLO, WORLD!'
+print(a.lower())
+
+print('Example 4')
+a = 'hello, world!'
+print(a.upper())
+
+print('Example 5')
+a = 'Day7, bone'
+print(a.replace('b','d'))
+
+print('Example 6')
+a = 'Hello, World!'
+print(a.split(',')) # returns ['Hello', ' World!']
+
