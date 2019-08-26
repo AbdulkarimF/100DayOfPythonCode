718ec7c744aaf3063412653df136faed613bf8de
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-08-26 21:50:29 +0300

    Python strings 1

---
+++ Day7.py
@@ -0,0 +1,25 @@
+# #Python Strings
+#1st Example
+print("Example 1")
+print("Hello")
+print('Hello')
+#2nd Example
+print("Example 2")
+a = "Hello"
+print(a)
+#3rd Example
+print("Example 3")
+a = """My name is Abdulkarim,
+My age is 21 years old,
+I'm studying in electrical engineering department
+in umm al-qura university."""
+print(a)
+#4th Example
+print("Example 4")
+a = "Hello, World!"
+print(a[1])
+#5th Example
+print("Example 5")
+b = "Hello, World!"
+print(b[2:5])
+
