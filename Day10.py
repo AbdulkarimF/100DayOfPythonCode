e694b31053854c5acbb207fa79172d2c90d0d7d5
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-08-27 22:47:05 +0300

    Python Operators 1

---
+++ Day10.py
@@ -0,0 +1,25 @@
+# Python operators
+# Python Arithmetic Operators
+print('Example 1 (Python Arithmetic Operators)')
+x = 5
+y = 10
+print(x * y)
+print(x+y)
+print(x-y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+# Python Assignment Operators
+print('Example 2 (Python Assignment Operators)')
+x = 10
+x += 1
+print(x)
+x -= 1
+print(x)
+# Python Comparison Operators
+print('Example 3 (Python Comparison Operators)')
+x = 10
+y = 100
+print(" x= 10, y=100, x > y ??")
+print(x>y)
