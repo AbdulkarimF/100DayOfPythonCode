fd4c0fab1e60661f59d7f7828047ae31726c52f8
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-03 11:37:43 +0300

    Python Lists 2

---
+++ Day14.py
@@ -0,0 +1,17 @@
+# Python Lists
+print('Example 1')
+letters = ['a', 'b', 'c', 'd', 'e']
+print(letters[1:4])
+# Check it Item Exists
+print('Example 2')
+letters = ['a', 'b', 'c', 'd', 'e']
+if 'a' in letters:
+    print("yes. 'a' is in the letters list")
+# Repeat Item in List
+print('Example 3')
+thislist = ["بايثون"] * 4
+print(thislist)
+# + Operator in List
+print('Example 4')
+addition = letters + thislist
+print(addition)
