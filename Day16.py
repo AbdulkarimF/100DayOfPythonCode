c087bb2b487949d5dbb52a63dcb63ecf3c316a99
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-05 04:40:22 +0300

    Python Tuples 1

---
+++ Day16.py
@@ -0,0 +1,37 @@
+# Python Tuples
+# Create a Tuple
+print('Examole 1')
+thistuple = ('apple', 'banana', 'cherry')
+print(thistuple)
+print('Examole 2')
+thistuple = ()
+print(thistuple)
+print('Examole 3')
+thistuple = (3,)
+print(thistuple)
+print('Examole 4')
+thistuple = (3, 1.3, 4.1, 7)
+print(thistuple)
+print('Examole 5')
+thistuple = ('apple', '1.1', 6, 'برمجة')
+print(thistuple)
+# Access Tuple Items
+print('Examole 6')
+thistuple = ('apple', 'banana', 'cherry')
+print(thistuple[2])
+# Loop Through a Tuple
+print('Examole 7')
+thistuple = ('apple', 'banana', 'cherry')
+for x in thistuple:
+    print(x)
+#print('Examole 8)
+# thistuple = ('apple', 'banana', 'cherry')
+# thistuple[3] = 'orange'
+# print(thistuple)
+#print('Examole 9)
+#thistuple = ('apple', 'banana', 'cherry')
+#del thistuple
+# print(thistuple)
+print('Examole 10')
+thistuple = ('apple', 'banana', 'cherry', 'orange')
+print(thistuple[0:2])
