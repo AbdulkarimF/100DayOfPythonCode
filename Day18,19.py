f2bc5b5a4697b5b6c754b65229bd7172e6e5ec7e
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-06 03:10:51 +0300

    Problem3

---
+++ Day18,19.py
@@ -0,0 +1,19 @@
+# Problem
+# 1st
+print('Problem 1')
+List = ['Engineering', 'Python', 3.98]
+List.append('Abdulkarim')
+print(List)
+List.pop(2)
+print(List)
+List.insert(0, 'student')
+print(List)
+newlist = List.copy()
+print(newlist)
+newlist.reverse()
+print(newlist)
+# 2nd
+print('Problem 2')
+tuple = ("java", "python", "swift")
+if 'python' in tuple:
+    print("yes, 'python' in the tuple")
