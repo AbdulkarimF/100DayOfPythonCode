e067e2418509cafd4bb3aba0e2e0c2a819da1cbc
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-17 15:17:53 +0300

    Python Loops 3

---
+++ Day30.py
@@ -0,0 +1,21 @@
+# Python Loops
+print("Example 1")
+for x in range(6):
+    print(x)
+print("Example 2")
+for x in range(2,6):
+    print(x)
+print("Example 3")
+for x in range(2, 30, 3):
+    print(x)
+print("Example 4")
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+print("Example 5")
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
