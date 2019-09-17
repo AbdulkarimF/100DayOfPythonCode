78ce89be63d5714ec85fff2e037e011291cf0d5a
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-17 09:38:11 +0300

    Python Loops 1

---
+++ Day28.py
@@ -0,0 +1,27 @@
+# Python Loops
+print('Example 1')
+i = 1
+while i < 6:
+    print(i)
+    i +=1
+print('Example 2')
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i +=1
+print('Example 3')
+i = 0
+while i < 6:
+    i +=1
+    if i == 3:
+       continue
+    print(i)
+print('Example 4')
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
