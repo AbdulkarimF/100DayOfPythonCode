0254f9d05e6fb30f947e8a45f58cd5eff23dc9cf
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-17 14:56:36 +0300

    Python Loops 2

---
+++ Day29.py
@@ -0,0 +1,26 @@
+# Python Loops
+print("Example 1")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+print("Example 2")
+for x in "Abdulkarim":
+    print(x)
+print("Example 3")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x== "banana":
+        break
+print("Example 4")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x== "banana":
+        break
+    print(x)
+print("Example 5")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x== "banana":
+        continue
+    print(x)
