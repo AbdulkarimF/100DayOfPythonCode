41a25eb2447fa94e89a21276f9671aaef9f8e03f
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-19 13:48:14 +0300

    Python Functions 1

---
+++ Day31.py
@@ -0,0 +1,18 @@
+# Python Functions:
+print("Example 1")
+def my_function():
+    print("Hello from func.")
+my_function()
+print("\nExample 2")
+def my_func(fname):
+    print(fname+ "refsnes")
+my_func("Email")
+my_func("Tobias")
+my_func("Linus")
+print("\nExample 3")
+def function(country = "Saudi Arabia"):
+    print("I am from " + country)
+function("UAE")
+function("USA")
+function()
+function("Egypt")
