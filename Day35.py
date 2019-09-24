8941db80a5a0158e4c63741aa737e53e61790947
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-24 21:11:18 +0300

    Pythom Lambda 1

---
+++ Day35.py
@@ -0,0 +1,10 @@
+# Python Lambda:
+print("Example 1")
+x = lambda a : a+10
+print(x(5))
+print("\nExample 2")
+x = lambda a, b : a*b
+print(x(10,5))
+print("\nExample 3")
+x = lambda a,b ,c : a+b-c
+print(x(2,3,6))
