58b9959d69d09b4bae4c4805753edd2958a2394e
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-27 21:40:36 +0300

    Python Arrays 1

---
+++ Day37.py
@@ -0,0 +1,16 @@
+# Python Array
+print('Example 1')
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+print('\nExample 2')
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[2]
+print(x)
+print('\nExample 3')
+cars = ["Ford", "Volvo", "BMW"]
+cars[1]="Honda"
+print(cars)
+print('\nExample 4')
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
