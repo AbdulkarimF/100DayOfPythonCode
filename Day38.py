57a648e5ca5ada8934e9a098d03ce811038d1b6d
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-09-27 21:48:49 +0300

    Python Arrays 2

---
+++ Day38.py
@@ -0,0 +1,17 @@
+# Python Arrays
+print('Example 1')
+cars = ["Ford", "Honda", "BMW"]
+for x in cars:
+    print(x)
+print('\nExample 2')
+cars = ["Ford", "Honda", "BMW"]
+cars.append("KIA")
+print(cars)
+print('\nExample 3')
+cars = ["Ford", "Honda", "BMW"]
+cars.pop(0)
+print(cars)
+print('\nExample 4')
+cars = ["Ford", "Honda", "BMW"]
+cars.remove("Ford")
+print(cars)
