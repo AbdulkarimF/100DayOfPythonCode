b37db3d999beede72cae03e31e07c36687ca9f8f
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-10-05 01:52:08 +0300

    Python Iterators

---
+++ Day45.py
@@ -0,0 +1,55 @@
+# Python Iterators:
+print('Example 1')
+mytuple = ("apple", "banana", "cherry")
+myit= iter(mytuple)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print('\nExample 2')
+mystr = "apple"
+myit= iter(mystr)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print('\nExample 3')
+mytuple = ("apple", "banana", "cherry")
+for x in mytuple:
+    print(x)
+print('\nExample 4')
+mystr = "apple"
+for x in mystr:
+    print(x)
+print("\nExample 5")
+class MyNumbers:
+    def __iter__(self):
+        self.a =1
+        return self
+    def __next__(self):
+        x = self.a
+        self.a +=1
+        return x
+myclass = MyNumbers()
+myiter= iter(myclass)
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print('\nExample 6')
+class MyNumbers:
+    def __iter__(self):
+        self.a =1
+        return self
+    def __next__(self):
+        if self.a <=20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+myclass = MyNumbers()
+myiter = iter(myclass)
+for x in myiter:
+    print(x)
