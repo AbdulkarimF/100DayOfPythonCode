3d276c8bbc3fb24263049d60395eaaa5851ba11d
AbdulkarimF <54324573+AbdulkarimF@users.noreply.github.com>
2019-11-03 00:52:00 +0300

    Problem 10

---
+++ Day67,68.py
@@ -0,0 +1,11 @@
+print("Enter your First and last letters of your name:")
+x = input()
+y = input()
+txt = "Your first letter is {} and last letter is {}."
+print(txt.format(x, y))
+
+print("\n\n")
+name = "Ahamad ali"
+Balance = 53.44
+txt = "Dear {}, your balance is {} $"
+print(txt.format(name, Balance))
